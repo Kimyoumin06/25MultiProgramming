a2f7fb5ef9ce4a8cd95a7db7ce84270abcfabebc
unknown <kimyoumin1221@gmail.com>
2025-04-15 13:04:34 +0900

    테스트

---
+++ STUDY/getch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	char c;
+	puts("press the key");
+	c = getch();
+	printf("pressed key : %c");
+	return 0;
+}
--- getch/get.c
+++ getch/get.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
+
 int main()
 {
 	char ch;
-	printf("press the key");
 	ch = getch();
-	printf("\npressed key : %c\n", ch);
-	
+
+	if (ch == 'c')
+	{
+		printf("press the c");
+	}
 	return 0;
 }
